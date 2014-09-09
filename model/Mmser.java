bae4a3a5d124429287c0efb04a06efe084df8afe
jarek <jarekpc@o2.pl>
2014-09-09 14:22:24 +0200

    pakiet model

---
+++ model/Mmser.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+/**
+ *
+ * @author admin
+ */
+public class Mmser {
+    String mmser_sergroup;
+    String mmser_serlast;
+
+    public Mmser() {
+    }
+
+    public String getMmser_sergroup() {
+        return mmser_sergroup;
+    }
+
+    public void setMmser_sergroup(String mmser_sergroup) {
+        this.mmser_sergroup = mmser_sergroup;
+    }
+
+    public String getMmser_serlast() {
+        return mmser_serlast;
+    }
+
+    public void setMmser_serlast(String mmser_serlast) {
+        this.mmser_serlast = mmser_serlast;
+    }
+    
+    
+}
---
+++ model/Upwmagwg.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author admin
+ */
+public class Upwmagwg {
+
+    String upwmagwg_zlecenie;
+    String upwmagwg_user;
+    String upwmagwg_nrpw;
+    Date upwmagwg_data;
+    /*
+    public Upwmagwg(String upwmagwg_zlecenie, String upwmagwg_user, String upwmagwg_nrpw, Date upwmagwg_data) {
+        this.upwmagwg_zlecenie = upwmagwg_zlecenie;
+        this.upwmagwg_user = upwmagwg_user;
+        this.upwmagwg_nrpw = upwmagwg_nrpw;
+        this.upwmagwg_data = upwmagwg_data;
+    }
+    */
+    public String getUpwmagwg_zlecenie() {
+        return upwmagwg_zlecenie;
+    }
+
+    public void setUpwmagwg_zlecenie(String upwmagwg_zlecenie) {
+        this.upwmagwg_zlecenie = upwmagwg_zlecenie;
+    }
+
+    public String getUpwmagwg_user() {
+        return upwmagwg_user;
+    }
+
+    public void setUpwmagwg_user(String upwmagwg_user) {
+        this.upwmagwg_user = upwmagwg_user;
+    }
+
+    public String getUpwmagwg_nrpw() {
+        return upwmagwg_nrpw;
+    }
+
+    public void setUpwmagwg_nrpw(String upwmagwg_nrpw) {
+        this.upwmagwg_nrpw = upwmagwg_nrpw;
+    }
+
+    public Date getUpwmagwg_data() {
+        return upwmagwg_data;
+    }
+
+    public void setUpwmagwg_data(Date upwmagwg_data) {
+        this.upwmagwg_data = upwmagwg_data;
+    }
+
+}
